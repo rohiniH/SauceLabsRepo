840e2671dd3cc4dc0fd721d2cc6ecc652fda525a
rohiniH <rohinihunnolli@gmail.com>
2026-07-06 08:29:59 +0530

    added comment

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,5 +1,6 @@
 import {test} from '@playwright/test'
 test("login",({page})=>{
+    //login code1
     await page.goto('https://www.saucedemo.com/');
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
