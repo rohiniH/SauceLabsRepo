136d48bc5c3dec5e68807332133ecdcadb6f5d7c
rohiniH <rohinihunnolli@gmail.com>
2026-07-08 07:19:33 +0530

    branch changesnew

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,9 @@
 import {test} from '@playwright/test'
-test("login",({page})=>{
+test("login",async({page})=>{
     //login code1
     await page.goto('https://www.saucedemo.com/');
     await page.getByPlaceholder('Username').fill('standard_user');
     await page.getByPlaceholder('Password').fill('secret_sauce');
     await page.getByRole('button',{name:'Login'}).click();
+    //branch1 changes new changes updates
 })
